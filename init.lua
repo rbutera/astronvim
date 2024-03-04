ba2fc9ba825e3c2b6f86ae7d4094c9a78059e5c6
Rai Butera <rai@rbutera.com>
2024-03-04 14:24:10 +0000

    add wsl nvim config

--- init.lua
+++ init.lua
@@ -156,5 +156,15 @@ return {
     vim.keymap.set("n", "<C-p>", function()
       require("telescope.builtin").find_files()
     end, { desc = "Search files" })
+    
+    if vim.fn.has('wsl') == 1 then
+      vim.api.nvim_create_autocmd('TextYankPost', {
+
+        group = vim.api.nvim_create_augroup('Yank', { clear = true }),
+        callback = function()
+          vim.fn.system('clip.exe', vim.fn.getreg('"'))
+        end,
+      })
+    end
   end,
 }
